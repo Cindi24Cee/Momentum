17edcca32ff7e3d4f59dfa01de32219c6e0b8f85
Cindi24Cee <masindisikhwari23@gmail.com>
2021-09-21 16:17:34 +0200

    Masindi Smtp

--- momes/Quote.aspx.cs
+++ momes/Quote.aspx.cs
@@ -67,49 +67,30 @@ namespace momes
 
         protected void btnSend_Click(object sender, EventArgs e)
         {
+
+
+
+
+
             try
             {
 
-                try
-                {
-                    //Initialize HTML to PDF converter 
-                    HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter(HtmlRenderingEngine.WebKit);
-                    WebKitConverterSettings settings = new WebKitConverterSettings();
-                    //Set WebKit path
-                    settings.WebKitPath = Server.MapPath("~/QtBinaries");
-                    //Assign WebKit settings to HTML converter
-                    htmlConverter.ConverterSettings = settings;
-                    //Get the current URL
-                    string url = HttpContext.Current.Request.Url.AbsoluteUri;
-                    //Convert URL to PDF
-                    PdfDocument document = htmlConverter.Convert(url);
-                    //Save the document
+                /*
+                 * Session["
+                 * 
+                 * 
+                 * 
+                 * 
+                 * 
+                 * 
+                 * 
+                 * */
+                Response.Redirect("Send_Quotees.aspx");
 
-                   // document.SaveProgress();
-                   // document.Save("Output.pdf", HttpContext.Current.Response, HttpReadType.Save);
-                }
-                catch
-                {
-                    throw;
-                }
-
-                SmtpClient smtpClient = new SmtpClient("mail.MyWebsiteDomainName.com", 25);
-
-                smtpClient.Credentials = new System.Net.NetworkCredential("zwonaka2020cee@gmail.com", "bondiS631@@");
-                // smtpClient.UseDefaultCredentials = true; // uncomment if you don't want to use the network credentials
-                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
-                smtpClient.EnableSsl = true;
-                MailMessage mail = new MailMessage();
-
-                //Setting From , To and CC
-                mail.From = new MailAddress("zwonaka2020cee@gmail.com", "MyWeb Site");
-                mail.To.Add(new MailAddress("info@MyWebsiteDomainName"));
-                mail.CC.Add(new MailAddress("MyEmailID@gmail.com"));
-                mail.Body = "b";
-                smtpClient.Send(mail);
+            }
 
 
-            }catch(Exception ex)
+            catch(Exception ex)
             {
                 ex.ToString();
             }
--- momes/Send_Quotees.aspx.cs
+++ momes/Send_Quotees.aspx.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Data_Access_Layer;
+using MimeKit;
 
 namespace momes
 {
@@ -13,7 +15,58 @@ namespace momes
         DBAccess da = new DBAccess();
         protected void Page_Load(object sender, EventArgs e)
         {
+            System.Net.ServicePointManager.ServerCertificateValidationCallback +=
+                   (s, cert, chain, sslPolicyErrors) => true;
+        }
 
+        protected void btnSend_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                MailMessage msg = new MailMessage();
+                msg.From = new MailAddress("zwonaka2020cee@gmail.com");
+                msg.To.Add(txtEmailAddress.Text);
+                msg.Subject = "Rental Quotation for "+  txtName.Text;
+                msg.Body = txtName.Text + " Hey.";  // Details From the other page
+                msg.IsBodyHtml = true;
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+
+                System.Net.NetworkCredential netCred = new System.Net.NetworkCredential();
+                netCred.UserName = "sekhwarimasindi21@gmail.com";
+                netCred.Password = "password";
+
+                smtp.UseDefaultCredentials = true;
+                smtp.Credentials = netCred;
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                smtp.Send(msg);
+
+                Label1.Text = "Successfully";
+              
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+
+            /* SmtpClient smtpClient = new SmtpClient("zwonaka2020cee@gmail.com", 25);
+
+              smtpClient.Credentials = new System.Net.NetworkCredential("zwonaka2020cee@gmail.com", "bondiS631@@");
+               smtpClient.UseDefaultCredentials = true; // uncomment if you don't want to use the network credentials
+              smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+              smtpClient.EnableSsl = true;
+              MailMessage mail = new MailMessage();
+
+              //Setting From , To and CC
+              mail.From = new MailAddress("zwonaka2020cee@gmail.com", "MyWeb Site");
+              mail.To.Add(new MailAddress(txtEmailAddress.Text));
+              mail.CC.Add(new MailAddress("masindisikhwari23@gmail.com"));
+              mail.Body = "Dear " +txtName.Text+ " Testing 1 2 3";
+              smtpClient.Send(mail);*/
         }
     }
 }
